023c60229f2f696394c417cd82ea406ee64aad31
Alireza Safari <alireza.safaree@gmail.com>
2025-04-08 15:05:51 +0330

    Implement logging in custom exception handler for improved error tracking

--- app/exceptions/exception_handlers.py
+++ app/exceptions/exception_handlers.py
@@ -1,9 +1,13 @@
+import logging
 from fastapi import Request
 from fastapi.responses import JSONResponse
 from app.exceptions.base import BaseException
 
+logger = logging.getLogger(__name__)
 
 async def custom_exception_handler(request: Request, exc: BaseException):
+    logger.error(f"{exc.key} | {exc.message} | {exc.errors}")
+    
     return JSONResponse(
         status_code=exc.code,
         content={"message": exc.message, "errors": exc.errors, "key": exc.key},
