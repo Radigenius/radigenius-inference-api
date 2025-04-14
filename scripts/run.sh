8b226f90d6ea8a44c70c50fdcb46a9ffcea65f2b
Alireza Safari <alireza.safaree@gmail.com>
2025-04-14 19:51:17 +0330

    Update run.sh to include pulling latest changes and installing dependencies
    - Added commands to pull the latest changes from the main branch.
    - Included installation of dependencies from requirements.txt before running the server.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,16 @@
 cd /workspace/radigenius-inference-api
 
+# pulling the latest changes
+echo "Pulling the latest changes..."
+git pull origin main
+
 # activating the virtualenv
 echo "Activating the virtualenv..."
 source venv/bin/activate
+
+# installing the dependencies
+echo "Installing the dependencies..."
+pip install -r requirements.txt
 
 # running the server
 echo "Running the server..."
